c61580c816492ab6e6acd3aad69e2d124d401ce7
gorilla <gorilla.ohgiraffers@gmail.com>
2024-06-18 13:19:16 +0900

    etc

--- chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
+++ chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
@@ -1,11 +1,17 @@
 package com.ohgiraffers.chap07thymeleaf.controller;
 
 import com.ohgiraffers.chap07thymeleaf.model.dto.MemberDTO;
+import com.ohgiraffers.chap07thymeleaf.model.dto.SelectCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("lecture")
@@ -20,12 +26,29 @@ public class LectureController {
 
     @GetMapping("conditional")
     public ModelAndView conditional(ModelAndView mv){
+        mv.addObject("num", 1);
+        mv.addObject("str", "사과");
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        memberDTOList.add(new MemberDTO("홍길동", 19, '남', "서초구"));
+        memberDTOList.add(new MemberDTO("홍길순", 20, '여', "강남구"));
+        memberDTOList.add(new MemberDTO("홍동길", 25, '남', "관악구"));
+        memberDTOList.add(new MemberDTO("홍도윤", 29, '여', "노원구"));
+        memberDTOList.add(new MemberDTO("홍묵", 21, '남', "중구"));
+        memberDTOList.add(new MemberDTO("홍보고", 26, '남', "서초구"));
+        mv.addObject("memberList", memberDTOList);
 
         mv.setViewName("/lecture/conditional");
+
         return mv;
     }
 
     @GetMapping("etc")
-    public void etc(){
+    public ModelAndView etc(ModelAndView mv){
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
+        mv.addObject("selectCriteria", selectCriteria);
+
+
+        mv.setViewName("/lecture/etc");
+        return mv;
     }
 }
