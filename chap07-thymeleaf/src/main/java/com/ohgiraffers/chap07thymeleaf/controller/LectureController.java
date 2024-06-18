015d70942522abfa975517e8f066a45bae9fa938
gorilla <gorilla.ohgiraffers@gmail.com>
2024-06-18 11:06:26 +0900

    thyemeleaf

---
+++ chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.chap07thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("lecture")
+public class LectureController {
+
+    @GetMapping("expression")
+    public ModelAndView expression(ModelAndView mv){
+        mv.setViewName("lecture/expression");
+        return mv;
+    }
+
+    @GetMapping("etc")
+    public void etc(){
+    }
+}
